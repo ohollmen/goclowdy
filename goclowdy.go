3bb1a07f2a2ae71f721a9a34eb52980fb71b05cb
Olli Hollmen <olli.hollmen@gmail.com>
2023-03-20 22:15:54 -0700

    Add package file taht declares the module (named) package. Seems package main in another file (in same dir) does not prevent compilations, reuse as a module (as long as one go file declares a package by modules name). Tested also: grsc.go comiles fine in (tested) utils/ subdir and is even better out-of-the-way (Plan B).

---
+++ goclowdy.go
@@ -0,0 +1,4 @@
+// Go package system wants this: complaint from utilizing module:
+// module goclowdy@latest found (v0.0.0-00010101000000-000000000000, replaced by ../goclowdy), but does not contain package goclowdy
+package goclowdy
+
