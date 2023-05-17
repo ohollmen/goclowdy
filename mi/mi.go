57de5a8a8e184da3b183f4db76be05a25f2e4964
Olli Hollmen <olli.hollmen@gmail.com>
2023-05-17 10:19:22 -0700

    Ad MI storage location, explicit deletion-ok flag. Simplify Delete interface (also many other interfaces). Complete CreateFrom (OLD:Create) method.

--- mi/mi.go
+++ mi/mi.go
@@ -7,6 +7,7 @@ import (
   "os"
   "fmt" // for name
   "context"
+  //"strings" // ??
 )
 // Machine Image Client Config
 type CC struct {
@@ -15,12 +16,16 @@ type CC struct {
   TZn string
   WD_keep int
   KeepMinH int // dur_keep_all => KeepMinH
+  KeepMaxH int
   // Priv
   tnow time.Time
   tloc * time.Location
+  StorLoc string
   //tnow Time
   // ctx !!
   c * compute.MachineImagesClient
+  DelOK bool
+  Debug bool
 }
 
 
@@ -48,6 +53,8 @@ func (cfg * CC) Init() int {
   var err error
   cfg.c, err = compute.NewMachineImagesRESTClient(ctx)
   if err != nil { return 1 }
+  if cfg.StorLoc == "" { cfg.StorLoc = "us"; }
+  //OLD: cfg.DelOK = false
   return 0
 }
 
@@ -56,6 +63,7 @@ func (cfg * CC) Classify(mi * computepb.MachineImage) int {
   nd := cfg.tnow.Sub(t) // Duration
   if err != nil { return KEEP_SAFE; }
   hrs := nd.Hours()
+  // TODO: Use cfg.KeepMaxH
   if hrs > (24 * (365 + 7)) { return DEL_1Y; } // fmt.Println("DEL ");
   // Less than MAX period (e.g. 1Y+1W) old ... but
   // Test for always keep-period
@@ -67,59 +75,85 @@ func (cfg * CC) Classify(mi * computepb.MachineImage) int {
   return DEL_1W
 }
 
-// To delete .. based on class.
+// To delete .. based on keep-classification.
 func ToBeDeleted(cl int) bool {
   if (cl >= DEL_1W) || (cl <= DEL_1Y) { return true }
   return false
 }
 
 // 
-func (cfg * CC) Delete(mi * computepb.MachineImage, c * compute.MachineImagesClient) {
+func (cfg * CC) Delete(mi * computepb.MachineImage) error { // , c * compute.MachineImagesClient
+  ctx := context.Background()
   fmt.Println("Should delete "+ mi.GetName());
-  //dreq := &computepb.DeleteMachineImageRequest{MachineImage: mi.GetName(), Project: project} //RequestId
+  dreq := &computepb.DeleteMachineImageRequest{MachineImage: mi.GetName(), Project: cfg.Project} //RequestId
   //dreq.Reset()
-  //op, err := c.Delete(ctx, dreq)
-  //if err != nil {}
-  //err = op.Wait(ctx)
-  //if err != nil {}
+  op, err := cfg.c.Delete(ctx, dreq)
+  if err != nil { fmt.Printf("Failed to delete MI: %s (%v) ", mi.GetName(), err); return err; }
+  err = op.Wait(ctx)
+  if err != nil { fmt.Println("Error waiting for MI Deletion of ", mi.GetName()); return err; }
+  fmt.Println("Success deleting MI: ", mi.GetName())
+  return nil
 }
 
 // inst * computepb.Instance (Instead of mini, make variadic ?)
-func (cfg * CC) Create(mini * string, c * compute.MachineImagesClient) { // , cfg * CC
+// OLD: mini * string
+// TODO: Allow options: altsuff, force
+func (cfg * CC) CreateFrom(inst * computepb.Instance, altsuff string) error { // , cfg * CC c * compute.MachineImagesClient
   
   var storageLocation []string
-  storageLocation = append(storageLocation, "us")
-  //mini := StdName(inst.GetName())
-  instance_path := "" // pass: Instance
+  storageLocation = append(storageLocation, cfg.StorLoc) // "us"
+  var imgname string
+  // Figure out MI name
+  if altsuff == "" { imgname = StdName(inst.GetName())
+  } else { imgname = inst.GetName() + "-" + altsuff }
+  // Check existing. This is good no matter what for clarity and possibly avoiding creation call
+  mi := cfg.GetOne(imgname)   // , cfg.c
+  if mi != nil  {
+    fmt.Println("Found (name-overlapping) MI: ", imgname)
+    // Skip (What to return)
+    if cfg.DelOK == false { fmt.Println("Skipping MI creation becase MI exists and no Forcing is on."); return nil; }
+    // or Delete (on DelOK/force)
+    err := cfg.Delete(mi)
+    if err != nil { fmt.Printf("Tried deleting MI with overlapping name (%s), but failed: %v\n", imgname, err); return err; }
+  }
+  //NONEED:instance_path := cfg.instpath(inst) // pass: Instance
+  instance_path := inst.GetSelfLink()
+  // Orig examples propose closing of client here. is that necessary ?
+  // This would imply a full re-instantiation / re-config of client
+  // Likely because client is established in the orig. func scope.
+  //defer cfg.c.Close()
   req := &computepb.InsertMachineImageRequest{
     Project: cfg.Project,
     SourceInstance: &instance_path,
     MachineImageResource: &computepb.MachineImage{
-      Name: mini, //&imageName,
+      Name: &imgname,
       SourceInstance: &instance_path,
       StorageLocations: storageLocation,
     },
   }
   ctx := context.Background()
-  op, err := c.Insert(ctx, req) // cfg.c
-  instancename := *mini
+  op, err := cfg.c.Insert(ctx, req) // cfg.c
+  // instancename := inst.GetName() // mini
   if err != nil {
-    fmt.Printf("Error in Insert - vm image failed for instance %s - %v\n", instancename , err)
-    //return(err)
+    fmt.Printf("Error in Insert - vm image failed for instance %s - %v\n", inst.GetName() , err)
+    return(err)
   }
   err = op.Wait(ctx)
   if err != nil {
-    fmt.Printf("Error in Wait(ctx) - vm image failed for instance %s - %v\n", instancename , err)
-    //return(err)
+    fmt.Printf("Error in Wait(ctx) - vm image failed for instance %s - %v\n", inst.GetName() , err)
+    return(err)
   }
+  fmt.Println("Success creating MI: ", imgname) // on debug
+  return nil
 }
 
-func (cfg * CC) GetOne(in string, c * compute.MachineImagesClient) *computepb.MachineImage {
+// OLD: , c * compute.MachineImagesClient
+func (cfg * CC) GetOne(in string) *computepb.MachineImage {
   ctx := context.Background()
   req := &computepb.GetMachineImageRequest{MachineImage: in, Project: cfg.Project}
   // https://pkg.go.dev/cloud.google.com/go/compute/apiv1#MachineImagesClient.Get
-  mi, err := c.Get(ctx, req)
-  if err != nil { fmt.Println("Error fetching i"); return nil }
+  mi, err := cfg.c.Get(ctx, req)
+  if err != nil { fmt.Println("Error fetching ", in); return nil }
   return mi
 }
 // ...StdName(i.GetName()). TODO: Opt date
@@ -129,12 +163,13 @@ func StdName(mn string) string {
   dtstr := fmt.Sprintf("%d-%.2d-%.2d",y, int(m), d)
   return mn+"-"+dtstr
 }
-
-//func instpath(inst *computepb.Instance, project string) string {
-//  return fmt.Sprintf( "%s/%s/%s/%s/%s/%s", "projects", project, "zones", instzone(inst), "instances", inst.GetName() )
+// Does VM have this ? top-level: self_link
+//func (cfg * CC) instpath(inst *computepb.Instance) string { // project string
+//  // OLD: project
+//  return fmt.Sprintf( "projects/%s/zones/%s/instances/%s", cfg.Project, InstZone(inst), inst.GetName() )
 //}
-
-//func instzone(inst *computepb.Instance) string {
+// TODO: Inst meth
+//func InstZone(inst *computepb.Instance) string {
 //  fullzone := inst.GetZone()
 //  li := strings.LastIndex(fullzone, "/")
 //  return fullzone[li+1:len(fullzone)]
